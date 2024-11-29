e300d8047b867963e5035735043bafbadf7d7cca
Ariwa <116925898+Ariwa042@users.noreply.github.com>
2024-11-29 15:46:49 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -19,7 +19,7 @@ load_dotenv()  # This loads environment variables from a .env file
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_URL = 'https://escrow-trade.onrender.com'
+BASE_URL = 'https://escrow-swap.onrender.com'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -30,7 +30,7 @@ SECRET_KEY = '5w5!86hnsccpm3c1zg2agjty25@^5rxjomj2#n-@gk&dyj8zi4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['escrow-trade.onrender.com','escrowtrade.site', 'www.escrowtrade.site', 'www.escrow-trade.onrender.com']
+ALLOWED_HOSTS = ['escrow-swap.onrender.com', 'escrow-trade.onrender.com','https://escrow-swap.onrender.com', 'escrowtrade.site', 'www.escrowtrade.site', 'www.escrow-trade.onrender.com']
 
 CSRF_TRUSTED_ORIGINS = [BASE_URL]
 
